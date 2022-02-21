bb75681875c873d903030518ddac2ba8371cc4e1
Diegolima31 <98998503+Diegolima31@users.noreply.github.com>
2022-02-20 23:16:30 -0300

    Java.Questoes

---
+++ Doc.Java1.java
@@ -0,0 +1,40 @@
+Doc.Java1
+public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("n");
+        double n = scanner.nextDouble();
+
+        if ( n == 1) {
+            System.out.println("     *");
+        }
+if ( n == 2) {
+            System.out.println("     **");
+        }
+ }
+if ( n == 3) {
+            System.out.println("     ***");
+        }
+ }
+if ( n == 4) {
+            System.out.println("     ****");
+        }
+ }
+if ( n == 5) {
+            System.out.println("     *****");
+        }
+ }
+if ( n == 6) {
+            System.out.println("     ******");
+        }
+if(senha.length() < 6)
+
+{
+
+int sum = 6 - senha.length();
+
+System.out.print(sum);
+
+}
